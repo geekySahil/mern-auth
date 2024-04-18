2e8d12640e7137e0cac5dc1d7701e4ab713b89df
geekySahil <ansari24sahil@gmail.com>
2024-04-19 00:35:23 +0530

    cors fixed

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ mongoose.connect(process.env.MONGODB_URI)
 })
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://mern-auth-mee5.onrender.com'],
   credentials: true
 }))
 
